d60b6fa8dd432704ef51c78bdd540e47d8208c8b
QiyuanChen <chenqiyuan1012@gmail.com>
2026-03-29 20:10:29 +0800

    feat(web): add pagination and tooltip UI components
    - Add shadcn/ui pagination component with ellipsis support
    - Add radix-ui tooltip component with custom styling
    - Fix button variant to exclude translation for haspopup buttons
    - Add TooltipProvider to root layout

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -4,6 +4,7 @@ import { cookies } from "next/headers";
 import { getLocale } from "next-intl/server";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ThemeScript } from "@/components/providers/theme-script";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { defaultLocale } from "@/lib/i18n";
 import { getThemeMode, themeCookieName } from "@/lib/theme";
 import "./globals.css";
@@ -44,7 +45,7 @@ export default async function RootLayout({
       <body className="min-h-full flex flex-col">
         <ThemeScript initialThemeMode={initialThemeMode} />
         <ThemeProvider initialThemeMode={initialThemeMode}>
-          {children}
+          <TooltipProvider>{children}</TooltipProvider>
         </ThemeProvider>
       </body>
     </html>
